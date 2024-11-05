80e6ae37038c6963369c3e905243a8e2543c4f7c
Vladislav Li <vladislav.li911@gmail.com>
2024-11-05 12:58:46 +0500

    Refactor layout components and remove unused pages

--- resources/js/Layouts/RootLayout.tsx
+++ resources/js/Layouts/RootLayout.tsx
@@ -1,10 +1,15 @@
+import { Link } from '@inertiajs/react';
+
 type Props = {
     children: React.ReactNode;
 };
 export default function RootLayout({ children }: Props) {
     return (
         <div className="flex min-h-dvh flex-col">
-            <header className="h-10">Header</header>
+            <header className="h-10">
+                <Link href="/">Home</Link>
+                <Link href="/about">About</Link>
+            </header>
             <div className="flex-1">{children}</div>
             <footer className="h-10">Footer</footer>
         </div>
